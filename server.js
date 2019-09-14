9ad84319b8db7edd40325f47bd72e6066f4cb3b2
Andres Ruiz-Velediaz <aruiz858@gmail.com>
2019-09-14 10:55:01 -0700

    fixed some server variables

--- server.js
+++ server.js
@@ -2,28 +2,28 @@ var express = require("express");
 var path = require("path");
 
 var app = express();
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Create New Characters - takes in JSON input
-app.post("/api/reservation", function(req, res) {
+app.post("/api/reservation", function (req, res) {
   // req.body hosts is equal to the JSON post sent from the user
   // This works because of our body parsing middleware
   var newReservation = req.body;
 
   // Using a RegEx Pattern to remove spaces from newCharacter
   // You can read more about RegExpression Patterns later https://www.regexbuddy.com/regex.html
-  ewReservation.routeName = ewReservation.name.replace(/\s+/g, "").toLowerCase();
+  newReservation.routeName = ewReservation.name.replace(/\s+/g, "").toLowerCase();
 
-  console.log(ewReservation);
+  console.log(newReservation);
 
-  characters.push(ewReservation);
-  res.json(ewReservation);
+  characters.push(newReservation);
+  res.json(newReservation);
 });
 
 
-app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
-  });
+app.listen(PORT, function () {
+  console.log("App listening on PORT " + PORT);
+});
