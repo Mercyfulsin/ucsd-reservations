41e441dec302599abc1020965b09d13585d90e87
Andres Ruiz-Velediaz <aruiz858@gmail.com>
2019-09-14 11:10:49 -0700

    Added random quote option to alert user

--- data.js
+++ data.js
@@ -1,19 +1,26 @@
 //To be added to HTML, sends obj via POST to server.
-<script type="text/javascript">
-    $("#add-btn").on("click", function(event) {
-        event.preventDefault();
-      var newReservation = {
-        name: $("#name").val().trim(),
-    phoneNum: $("#phone-number").val().trim(),
-    email: $("#email").val().trim(),
-    uniqueId: $("#unique-id").val().trim()
-  };
 
-  // Question: What does this code do??
-  $.post("/api/reservation", newReservation)
-        .then(function(data) {
-        console.log("add.html", data);
-    alert("Your table has been reserved, thank you");
-  });
+$("#add-btn").on("click", function (event) {
+    event.preventDefault();
+    var newReservation = {
+        name: $("#name").val().trim(),
+        phoneNum: $("#phone-number").val().trim(),
+        email: $("#email").val().trim(),
+        uniqueId: $("#unique-id").val().trim()
+    };
+
+    // Question: What does this code do??
+    $.post("/api/reserve", newReservation)
+        .then(function (data) {
+            console.log("add.html", data);
+            getQuote();
+        });
 });
-  </script>
+
+
+var quotes = ["","",""];
+function getQuote(){
+    var random = Math.round(Math.random*(quotes.length-1));
+    alert("Your table has been reserved, thank you\r"+
+    quotes[random]);
+}
--- server.js
+++ server.js
@@ -12,17 +12,13 @@ app.post("/api/reservation", function (req, res) {
   // req.body hosts is equal to the JSON post sent from the user
   // This works because of our body parsing middleware
   var newReservation = req.body;
-
   // Using a RegEx Pattern to remove spaces from newCharacter
   // You can read more about RegExpression Patterns later https://www.regexbuddy.com/regex.html
   newReservation.routeName = newReservation.name.replace(/\s+/g, "").toLowerCase();
-
   console.log(newReservation);
-
   characters.push(newReservation);
   res.json(newReservation);
 });
-
 
 app.listen(PORT, function () {
   console.log("App listening on PORT " + PORT);
